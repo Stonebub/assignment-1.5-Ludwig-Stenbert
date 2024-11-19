4012eaea6145748411be0899b2ff45bdd8a1150e
Stonebub <ludwig.s@hotmail.se>
2024-11-19 21:42:20 +0100

    uppgift 1.5

---
+++ assigment1.5 + LudwigStenbert/scripts/javascript.js
@@ -0,0 +1,20 @@
+
+
+function checkID() {
+
+//let vip = document.getElementById(vip).checked;
+//let age = parseInt( document.getElementById(age).value);
+
+let vip = true;
+let age = 17;
+
+if (vip) {
+  alert("yippie ur in");
+} else if (18 >= age) {
+  alert("välkommen");
+} else {
+  alert("get the fuck outta here");
+}
+
+}
+
